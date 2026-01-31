d25557837672d55db84b0ea1911e6618c48a9d4e
aminkammoun <aminekammou55@gmail.com>
2026-01-31 18:26:09 +0100

    upload recurring

--- src/modules/recurring/service/recurring.service.ts
+++ src/modules/recurring/service/recurring.service.ts
@@ -72,11 +72,11 @@ export class RecurringService {
                 npe03__Date_Established__c: recurring.dateEstablished,
                 npsp__Day_of_Month__c: recurring.DayOfMonth,
                 npsp__Status__c: recurring.status,
-                npsp__PaymentMethod__c: recurring.npsp__PaymentMethod__c,
-                npe03__Recurring_Donation_Campaign__c: recurring.npe03__Recurring_Donation_Campaign__c,
+                //npsp__PaymentMethod__c: recurring.npsp__PaymentMethod__c,
+                //npe03__Recurring_Donation_Campaign__c: recurring.npe03__Recurring_Donation_Campaign__c,
                 Donation_Source__c: 'Fundraising App',
-                Stripe_Customer__c: recurring.customerStripe || recurring.customerStipe,
-                Stripe_subscription_url__c: 'https://dashboard.stripe.com/acct_1S5xcLPK7Mt7pUeD/subscriptions/' + recurring.subscriptionStripe,
+                //Stripe_Customer__c: recurring?.customerStripe || recurring.customerStipe,
+               // Stripe_subscription_url__c: 'https://dashboard.stripe.com/acct_1S5xcLPK7Mt7pUeD/subscriptions/' + recurring.subscriptionStripe,
                 //RecordTypeId: donation.RecordTypeId,
             };
             const result = await handleInsertQuery('/services/data/v65.0/sobjects/', 'npe03__Recurring_Donation__c/', payload, token);
