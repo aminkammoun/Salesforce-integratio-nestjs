e2433b1896cef28f9b5e7787a98ecb03c2ffa812
aminkammoun <aminekammou55@gmail.com>
2026-02-23 17:41:56 +0100

    log date change to cst

--- src/modules/log/entities/log.entity.ts
+++ src/modules/log/entities/log.entity.ts
@@ -1,11 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
 @Schema()
-export class Log  extends Document{
+export class Log extends Document {
     @Prop()
     name: string;
 
-    @Prop()
+    @Prop({ default: () => new Date(new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' })) })
     date: Date;
 
     @Prop()
