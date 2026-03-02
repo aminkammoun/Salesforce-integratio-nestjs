57aebf0e0a7341346f41d221b1b2495e0480dfdd
aminkammoun <aminekammou55@gmail.com>
2026-03-03 00:53:41 +0100

    donation

--- src/modules/donation/service/donation.service.ts
+++ src/modules/donation/service/donation.service.ts
@@ -576,7 +576,9 @@ export class DonationService {
 
                         // 2. Insert Parent Opportunity
                         const result = await handleInsertQuery('/services/data/v65.0/sobjects/', 'Opportunity/', payload, token);
-                        console.log(result)
+                        if(result ){
+                            console.log(result);
+                        }
                         if (result?.salesforceId) {
                             const parentOppId = result.salesforceId;
 
