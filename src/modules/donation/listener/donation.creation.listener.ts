3a0b128c350794ef7ea8ce26ac92426be423159f
aminkammoun <aminekammou55@gmail.com>
2026-03-18 03:40:18 +0100

    donation

--- src/modules/donation/listener/donation.creation.listener.ts
+++ src/modules/donation/listener/donation.creation.listener.ts
@@ -10,6 +10,7 @@ export class DonationCreationListener {
     @OnEvent('donation.created')
     async handleDonationCreated(payload: any) {
         try {
+            this.logger.log(`Webhook sent for donation ${payload}`);
             const url = 'https://zrtext.dev.mwl.org/donation-created/';
             const headers = {
                 'Content-Type': 'application/json',
