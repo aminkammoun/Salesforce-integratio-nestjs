d703a2e6579744013c75dbd7d9375120f6da244a
aminkammoun <aminekammou55@gmail.com>
2026-06-27 21:40:43 +0100

    debug token

--- src/modules/donation/service/donation.service.ts
+++ src/modules/donation/service/donation.service.ts
@@ -1073,6 +1073,7 @@ export class DonationService {
     }
     async enterCash(campaignId: string, amount: number) {
         const token = await authenticateSalesforce();
+        console.log(token)
         const updatePayload = { Cash_Collected__c: amount };
         const result = await handleUpdateQuery('/services/data/v65.0/sobjects/Campaign', '', campaignId, updatePayload, token);
         return result;
