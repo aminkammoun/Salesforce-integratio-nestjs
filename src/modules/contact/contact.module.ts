780d57cd2ca927aec9d9d358c48a14509684d4d6
ZenrmAmine <amine.kammoun@zenrm.co>
2026-07-20 21:40:20 +0100

    delete neon on prod

--- src/modules/contact/contact.module.ts
+++ src/modules/contact/contact.module.ts
@@ -7,15 +7,17 @@ import { ContactService } from './service/contact.service';
 import { DonationModule } from '../donation/donation.module';
 import { SponsorshipModule } from '../sponsorship/sponsorship.module';
 import { RecurringModule } from '../recurring/recurring.module';
+import { NeonDatabaseControleModule } from 'src/config/neon.controle';
 @Module({
   imports: [
     DonationModule,
     SponsorshipModule,
     RecurringModule,
+    //NeonDatabaseControleModule,
     MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
   ],
   controllers: [ContactController],
   providers: [ContactService],
-  exports: [ContactService, MongooseModule],
+  exports: [ContactService, MongooseModule, NeonDatabaseControleModule],
 })
 export class ContactModule { }
